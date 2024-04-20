3eb4cf4d885cbcf3bf00d220e6405a9049a56ffd
Tony Huang <kuanlin830410@gmail.com>
2024-04-21 02:29:21 +0800

    init comm it

--- javascripts/all.js
+++ javascripts/all.js
@@ -58,23 +58,67 @@ $(function () {
 });
 
 //swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 0,
-  centeredSlides: true,
-  preventClicks:true,
-  loop:true,  
-  on: {
-    init: function () {
-      $('.swiper-slide').addClass('changed');
-    },
-    slideChangeTransitionStart : function() {
-      $('.swiper-slide').addClass('changing');
-      $('.swiper-slide').removeClass('changed');
-    },
-    slideChangeTransitionEnd : function() {
-      $('.swiper-slide').removeClass('changing');
-      $('.swiper-slide').addClass('changed');
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 'auto',
+//   spaceBetween: 0,
+//   centeredSlides: true,
+//   preventClicks:true,
+//   loop:true,  
+//   on: {
+//     init: function () {
+//       $('.swiper-slide').addClass('changed');
+//     },
+//     slideChangeTransitionStart : function() {
+//       $('.swiper-slide').addClass('changing');
+//       $('.swiper-slide').removeClass('changed');
+//     },
+//     slideChangeTransitionEnd : function() {
+//       $('.swiper-slide').removeClass('changing');
+//       $('.swiper-slide').addClass('changed');
+//     }
+//   },
+// });
+
+$(function () {
+  var galleryThumbs = new Swiper(".gallery-thumbs", {
+    centeredSlides: true,
+    centeredSlidesBounds: true, 
+    direction: "horizontal",
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: false,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    watchOverflow: true,
+    breakpoints: {
+      480: {
+        direction: "vertical",
+        slidesPerView: 3
+      }
     }
-  },
+  });
+  var galleryTop = new Swiper(".gallery-top", {
+    direction: "horizontal",
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    a11y: {
+      prevSlideMessage: "Previous slide",
+      nextSlideMessage: "Next slide",
+    },
+    keyboard: {
+      enabled: true,
+    },
+    thumbs: {
+      swiper: galleryThumbs
+    }
+  });
+  galleryTop.on("slideChangeTransitionStart", function () {
+    galleryThumbs.slideTo(galleryTop.activeIndex);
+  });
+  galleryThumbs.on("transitionStart", function () {
+    galleryTop.slideTo(galleryThumbs.activeIndex);
+  });
 });
