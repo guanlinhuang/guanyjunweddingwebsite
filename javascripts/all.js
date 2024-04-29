1aeafc6ee6591b8f908c184b89ab80ccf7999f27
Tony Huang <kuanlin830410@gmail.com>
2024-04-29 23:44:22 +0800

    init commit

--- javascripts/all.js
+++ javascripts/all.js
@@ -59,28 +59,6 @@ anime.timeline({ loop: 1 })
 
 //swiper
 
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 'auto',
-//   spaceBetween: 0,
-//   centeredSlides: true,
-//   preventClicks:true,
-//   loop:true,  
-//   on: {
-//     init: function () {
-//       $('.swiper-slide').addClass('changed');
-//     },
-//     slideChangeTransitionStart : function() {
-//       $('.swiper-slide').addClass('changing');
-//       $('.swiper-slide').removeClass('changed');
-//     },
-//     slideChangeTransitionEnd : function() {
-//       $('.swiper-slide').removeClass('changing');
-//       $('.swiper-slide').addClass('changed');
-//     }
-//   },
-// });
-// //https://codepen.io/ylem76/pen/VwbRGEg
-
 //相簿1
 $(function () {
   var galleryThumbs = new Swiper(".gallery-thumbs", {
@@ -90,7 +68,7 @@ $(function () {
     //loop: true, //會在原本slide前後複製若干個slide(預設一個)並在適當的時候切換，讓Swiper看起來是循環的
     direction: "horizontal", //Slides的滑动方向，可设置水平(horizontal)或垂直(vertical)
     spaceBetween: 3, //slide之間的距離
-    slidesPerView: 4, //設定slider容器能夠同時顯示的slides數量
+    slidesPerView: 3, //設定slider容器能夠同時顯示的slides數量
     freeMode: false, //slide滑動時只滑動一格，並自動貼合wrapper
     watchSlidesVisibility: true,
     watchSlidesProgress: true,
@@ -129,6 +107,7 @@ $(function () {
   });
 });
 //https://codepen.io/rogerkuik/pen/abZOLXr
+//https://codepen.io/ylem76/pen/VwbRGEg
 
 //相簿2
 $(function () {
@@ -230,12 +209,14 @@ $(function () {
 function GoogleForm() { //這裡要對應到自己的 javascript 名稱
   //宣告欄位
   var field1 = $("[name='Name']").val();
-  var field2 = $("[name='Text']").val();
+  var field2 = $("[name='Connection']").val();
+  var field3 = $("[name='Text']").val();
   $.ajax({
    url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSf_j1pL30Hab_ETr0-ACaueuS4xUraGPiPOzelKTZBEmXSymw/formResponse", //Google Form 裡面的 form action 網址 ＊＊記得要填＊＊
    data: { //Google Form 裡面的欄位 name ＊＊記得要改＊＊
     "entry.565983712": field1,
-    "entry.617255079": field2
+    "entry.2119774223":field2,
+    "entry.1498531925": field3
    },
    type: "POST",
    dataType: "xml",
